c9c5c01b6ff59640a0937484f7a2ae7bbe5b890e
Paul Charles <santosh@ku.edu>
2016-11-21 01:08:43 -0600

    Add Source and Header Files
    Added Morse_Code.cpp, Morse_Code.h, and main.cpp

---
+++ Morse_Code.cpp
@@ -0,0 +1,7 @@
+#include <string>
+#include <iostream>
+#include <iomanip>
+#include "Morse_Code.h"
+
+using namespace std;
+
---
+++ main.cpp
@@ -0,0 +1,14 @@
+#include <string>
+#include <sstream>
+#include "Morse_Code.h"
+#include<iostream>
+
+using namespace std;
+int main()
+{
+	MorseCode obj;
+	string in, str;
+
+	
+
+}
